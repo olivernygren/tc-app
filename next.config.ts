a4d216e647513fca9179243360adf5255bbc459f
Oliver Nygren <oliver@jobnet.se>
2024-12-02 20:10:32 +0100

    Configure eslint, prettier and i18n

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+
+import type { NextConfig } from 'next';
+
+const { i18n } = require('./next-i18next.config');
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  compiler: {
+    styledComponents: {
+      ssr: true,
+    },
+  },
+  i18n,
 };
 
 export default nextConfig;
