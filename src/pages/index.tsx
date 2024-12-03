ba771766ab8e593c0402241dfa2234d5942f79a2
Oliver Nygren <oliver@jobnet.se>
2024-12-03 19:13:53 +0100

    Configure root layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,37 @@
-import localFont from 'next/font/local';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
 import TCHead from '@/components/head/TCHead';
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  weight: '100 900',
-});
+import theme from '@/utils/theme';
 
 export async function getStaticProps(context: any) {
   return {
     props: {
+      users: [],
       ...(await serverSideTranslations(context.locale, ['common'])),
     },
   };
 }
 
-export default function Home() {
+interface HomePageProps {
+  users: any;
+}
+
+export default function HomePage({ users }: HomePageProps) {
   const { t } = useTranslation('common');
+
+  console.log(users);
 
   return (
     <>
       <TCHead title="TC | Home" />
-      <div id="_next" className={geistSans.className}>
-        <h1>Hello, world!</h1>
-        <p>{t('test')}</p>
+      <div>
+        <h1 style={{ color: theme.colors.white }}>Hello, world!</h1>
+        <p style={{ color: theme.colors.white }}>{t('test')}</p>
+        {users?.map((user: any) => (
+          <div key={user.documentId}>
+            <p style={{ color: theme.colors.white }}>{user.email}</p>
+          </div>
+        ))}
       </div>
     </>
   );
